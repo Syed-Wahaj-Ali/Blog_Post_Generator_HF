58164323e0a285470d95b65bbb4f6d493f95b193
Syed Wahaj Ali Shah <syedwahajshah90@gmail.com>
2025-06-21 19:59:00 +0500

    Model Downloaded

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2',pad_token_id = tokenizer.eos_token_id )
+
+print('All Done')
